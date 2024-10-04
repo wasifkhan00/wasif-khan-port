c157e19171205964a10805803cd95db76e4e47b2
unknown <ukhanwasif@gmail.com>
2024-10-04 05:02:13 -0700

    Updated Data Home Screen

--- src/lib/data/home.ts
+++ src/lib/data/home.ts
@@ -3,9 +3,9 @@ import { getSkills } from './skills';
 
 export const title = 'Home';
 
-export const name = 'Name';
+export const name = 'Wasif Khan';
 
-export const lastName = 'LastName';
+export const lastName = '';
 
 export const description =
 	'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti, rerum. Debitis accusantium deleniti enim iste dignissimos? Similique, exercitationem! Odit vero, numquam quae ratione maxime sunt reiciendis laudantium quaerat iure ipsum!';
@@ -16,25 +16,10 @@ export const links: Array<{ platform: Platform; link: string }> = [
 		platform: Platform.Linkedin,
 		link: 'https://www.linkedin.com/'
 	},
-	{
-		platform: Platform.Twitter,
-		link: 'https://twitter.com/'
-	},
-	{
-		platform: Platform.StackOverflow,
-		link: 'https://stackoverflow.com/'
-	},
+
 	{
 		platform: Platform.Email,
 		link: 'riadh_adrani@hotmail.fr'
-	},
-	{
-		platform: Platform.Youtube,
-		link: 'https://www.youtube.com'
-	},
-	{
-		platform: Platform.Facebook,
-		link: 'https://www.facebook.com'
 	}
 ];
 
