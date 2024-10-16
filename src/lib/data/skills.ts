d3d62cd9c365d0d7de68a4eaeea8bca52c1668aa
unknown <ukhanwasif@gmail.com>
2024-10-16 10:06:25 -0700

    skill section has been completed

--- src/lib/data/skills.ts
+++ src/lib/data/skills.ts
@@ -56,7 +56,7 @@ This experience reinforced his belief in the importance of strong typing in mode
 		category: 'pro-lang'
 	}),
 	defineSkill({
-		slug: 'Python',
+		slug: 'Py',
 		color: 'yellow',
 		description:
 			'Wasif Khan has explored the Python programming language and, while he doesn’t yet have a strong foundation, he feels confident in his ability to work with it. Having written over 1,000 lines of code in Python, he finds that picking up the syntax comes easily to him. This familiarity gives him the assurance that he can handle projects requiring Python, and he is eager to further develop his skills in this versatile language. His enthusiasm for learning and adaptability make him ready to take on new challenges in Python programming.',
@@ -150,7 +150,7 @@ As he delved deeper into Express.js, Wasif began exploring databases and various
 		category: 'library'
 	}),
 	defineSkill({
-		slug: 'db',
+		slug: 'mongodb',
 		color: 'orange',
 		description: `MongoDB was one of the first databases Wasif Khan worked with, and he quickly became adept at creating RESTful APIs using Node.js and Express.js as the server. He found that while populating databases was relatively straightforward, the real challenge lay in designing an efficient architecture for API requests that balanced performance and cost-effectiveness.
 
@@ -159,90 +159,95 @@ Wasif learned the importance of managing data effectively, implementing strategi
 		name: 'MongoDB',
 		category: 'db'
 	}),
-	// Databases descrip has been updated beggining from below tomoroow
-	// devtools start
-	defineSkill({
-		slug: 'library',
-		color: 'orange',
-		description: `When Wasif Khan learned TypeScript, he also discovered that it was specifically designed to work seamlessly with AngularJS. Intrigued, he jumped into exploring AngularJS and its core concepts. As he delved deeper, he appreciated how AngularJS structured applications and facilitated the development of dynamic web interfaces.
 
-Wasif believes that just as learning programming in one language makes it easier to pick up others—due to shared core concepts—similarly, mastering one framework or library lays the groundwork for understanding others. His experience with TypeScript and AngularJS reinforced this idea, allowing him to transition smoothly between different tools and technologies. This approach not only accelerated his learning process but also enriched his overall development skill set, making him a more versatile and confident developer.`,
+	defineSkill({
+		slug: 'aws',
+		color: 'orange',
+		description: `Wasif Khan had a solid grasp of full stack engineering, but he soon realized that while coding backend architecture was straightforward, deploying it presented a new set of challenges. This prompted him to explore AWS, where he learned about deploying backend applications on Ubuntu via EC2 instances.
+
+He delved into essential concepts like load balancers and SSL certificates, understanding their roles in ensuring security. Wasif recognized that securing the backend was critical, as any vulnerabilities could potentially grant unauthorized access to the entire application. He focused on measures to protect against DDoS attacks and unauthorized data insertion, solidifying his knowledge of best practices in backend security. This exploration not only enhanced his technical skills but also deepened his understanding of the importance of robust security measures in full stack development.`,
 		logo: Assets.AWS,
 		name: 'AWS',
 		category: 'devtools'
 	}),
-	// devtools first item end
 
 	defineSkill({
-		slug: 'frameworks',
+		slug: 'docker',
 		color: 'orange',
-		description: `When Wasif Khan learned TypeScript, he also discovered that it was specifically designed to work seamlessly with AngularJS. Intrigued, he jumped into exploring AngularJS and its core concepts. As he delved deeper, he appreciated how AngularJS structured applications and facilitated the development of dynamic web interfaces.
+		description: `When it came to deploying his backend, Wasif Khan sought cost-effective solutions and delved into Docker. He understood that the concept was similar to installing an operating system from an ISO image, allowing him to package his code in a way that made it easily accessible for his team.
 
-Wasif believes that just as learning programming in one language makes it easier to pick up others—due to shared core concepts—similarly, mastering one framework or library lays the groundwork for understanding others. His experience with TypeScript and AngularJS reinforced this idea, allowing him to transition smoothly between different tools and technologies. This approach not only accelerated his learning process but also enriched his overall development skill set, making him a more versatile and confident developer.`,
+As he learned more about Docker, Wasif began contributing to open source repositories, recognizing Docker as an essential technology in modern web development. He appreciated how containerization streamlined the deployment process, enabling consistent environments and simplifying the management of dependencies. For him, the ability to encapsulate a web application within a Docker container represented a significant advancement, making it one of the best tools for deploying applications efficiently and reliably. This experience not only enhanced his technical skills but also connected him with the wider developer community.`,
 		logo: Assets.Docker,
 		name: 'Docker',
 		category: 'devtools'
 	}),
 	defineSkill({
-		slug: 'db',
+		slug: 'azure',
 		color: 'orange',
-		description: `When Wasif Khan learned TypeScript, he also discovered that it was specifically designed to work seamlessly with AngularJS. Intrigued, he jumped into exploring AngularJS and its core concepts. As he delved deeper, he appreciated how AngularJS structured applications and facilitated the development of dynamic web interfaces.
+		description: `Wasif Khan gained valuable experience with Azure DevOps while working at In-House.com, where he managed the company’s codebase and collaborated with a team of developers to ensure smooth delivery of code. In this agile environment, he learned the importance of continuous integration and continuous delivery (CI/CD), which streamlined the development process and enhanced collaboration.
 
-Wasif believes that just as learning programming in one language makes it easier to pick up others—due to shared core concepts—similarly, mastering one framework or library lays the groundwork for understanding others. His experience with TypeScript and AngularJS reinforced this idea, allowing him to transition smoothly between different tools and technologies. This approach not only accelerated his learning process but also enriched his overall development skill set, making him a more versatile and confident developer.`,
+Through Azure DevOps, Wasif explored various tools and practices that improved project management, such as version control, automated testing, and deployment pipelines. His hands-on experience helped him understand how to effectively manage workflows and maintain high-quality standards in software development. This role not only honed his technical skills but also reinforced his ability to work in a team-oriented, agile setting.`,
 		logo: Assets.azure,
 		name: 'Azure DevOps',
 		category: 'devtools'
 	}),
 	defineSkill({
-		slug: 'devtools',
+		slug: 'linux',
 		color: 'orange',
-		description: `When Wasif Khan learned TypeScript, he also discovered that it was specifically designed to work seamlessly with AngularJS. Intrigued, he jumped into exploring AngularJS and its core concepts. As he delved deeper, he appreciated how AngularJS structured applications and facilitated the development of dynamic web interfaces.
+		description: `Before diving into web development, Wasif Khan was deeply curious about ethical hacking. He explored Linux extensively, using it to perform tasks like capturing Wi-Fi handshakes and running dictionaries to crack passwords, which became one of his favorite areas of interest.
 
-Wasif believes that just as learning programming in one language makes it easier to pick up others—due to shared core concepts—similarly, mastering one framework or library lays the groundwork for understanding others. His experience with TypeScript and AngularJS reinforced this idea, allowing him to transition smoothly between different tools and technologies. This approach not only accelerated his learning process but also enriched his overall development skill set, making him a more versatile and confident developer.`,
+Although he eventually shifted his focus to web development, his passion for ethical hacking remained intact, particularly as he recognized the importance of Linux in deploying applications on AWS and within Docker containers. He continued to use Linux as a hobby for ethical hacking, allowing him to maintain a connection to that field while enhancing his overall technical skills. This background in security has also informed his approach to building secure applications in his web development work.`,
 		logo: Assets.linux,
 		name: 'Kali Linux',
 		category: 'devtools'
 	}),
 	defineSkill({
-		slug: 'soft',
+		slug: 'github',
 		color: 'orange',
-		description: `When Wasif Khan learned TypeScript, he also discovered that it was specifically designed to work seamlessly with AngularJS. Intrigued, he jumped into exploring AngularJS and its core concepts. As he delved deeper, he appreciated how AngularJS structured applications and facilitated the development of dynamic web interfaces.
+		description: `Wasif Khan developed a strong understanding of GitHub during his early days as a frontend developer. He hosted his first repository in 2021, which marked the beginning of his exploration of GitHub's features, such as branching and forking repositories.
 
-Wasif believes that just as learning programming in one language makes it easier to pick up others—due to shared core concepts—similarly, mastering one framework or library lays the groundwork for understanding others. His experience with TypeScript and AngularJS reinforced this idea, allowing him to transition smoothly between different tools and technologies. This approach not only accelerated his learning process but also enriched his overall development skill set, making him a more versatile and confident developer.`,
+This initial experience laid the foundation for his later contributions to open source projects. His active engagement with GitHub not only enhanced his technical skills but also helped him connect with the developer community. To this day, his GitHub account remains a testament to his ongoing journey in software development, showcasing his projects and contributions.`,
 		logo: Assets.Github,
 		name: 'Github',
 		category: 'devtools'
 	}),
 	defineSkill({
-		slug: 'skill',
+		slug: 'netlify',
 		color: 'orange',
-		description: `When Wasif Khan learned TypeScript, he also discovered that it was specifically designed to work seamlessly with AngularJS. Intrigued, he jumped into exploring AngularJS and its core concepts. As he delved deeper, he appreciated how AngularJS structured applications and facilitated the development of dynamic web interfaces.
+		description: `Wasif Khan has a solid understanding of Netlify, having deployed various web applications using its straightforward deployment methods. He appreciates how easy it is to connect Netlify with GitHub, allowing him to automatically fetch repositories and host them seamlessly.
 
-Wasif believes that just as learning programming in one language makes it easier to pick up others—due to shared core concepts—similarly, mastering one framework or library lays the groundwork for understanding others. His experience with TypeScript and AngularJS reinforced this idea, allowing him to transition smoothly between different tools and technologies. This approach not only accelerated his learning process but also enriched his overall development skill set, making him a more versatile and confident developer.`,
+Additionally, he enjoys the ability to create custom URLs for his deployed apps, which enhances their professionalism and usability. This experience with Netlify has made it one of his preferred tools for hosting web applications, further showcasing his versatility as a full stack developer.`,
 		logo: Assets.netlify,
 		name: 'Netlify',
 		category: 'devtools'
 	}),
 	defineSkill({
-		slug: 'hard',
+		slug: 'git',
 		color: 'orange',
-		description: ``,
+		description: `Wasif Khan learned about GitHub, but he soon recognized the importance of using Git as a version control system to deploy and push branches to repositories via the command line interface (CLI). This exploration opened up new efficiencies in his workflow, allowing him to manage changes effectively both in production and local environments.
+
+With Git, he could make changes locally, test them, and then push them to the repository seamlessly. This capability greatly eased his development process, enhancing collaboration with his team and ensuring that version control was handled smoothly. His understanding of Git has become a crucial part of his skill set, enabling him to work more efficiently and confidently in his projects.`,
 		logo: Assets.Git,
 		name: 'Git',
 		category: 'devtools'
 	}),
 	defineSkill({
-		slug: 'pro',
+		slug: 'postman',
 		color: 'orange',
-		description: ``,
+		description: `When developing APIs, Wasif Khan uses Postman as his primary tool for testing various types of requests, including POST, GET, PUT, and PATCH. His good understanding of Postman allows him to thoroughly test and debug his APIs effectively.
+
+Although he occasionally uses an extension in VS Code called Thunderbolt, he particularly appreciates the user-friendly interface of Postman. This ease of use enhances his workflow, making it simpler to manage and execute API requests. His proficiency with these tools reflects his commitment to ensuring that his APIs function correctly and efficiently.`,
 		logo: Assets.postman,
 		name: 'Postman',
 		category: 'devtools'
 	}),
 	defineSkill({
-		slug: 'api',
+		slug: 'Restfulapi',
 		color: 'orange',
-		description: ``,
+		description: `
+Wasif Khan has a strong understanding of RESTful APIs and has developed several static RESTful APIs to support the websites he’s built over time. He prioritizes security, ensuring that unauthorized access to the database is not permitted.
+
+His APIs are designed to be cost-efficient, capable of handling over a million requests. In fact, he has tested their performance with over 10 million requests using the Artillery library. Wasif implements middleware to safeguard against unauthorized source requests and takes measures to protect his APIs from DDoS attacks, demonstrating his commitment to building robust and secure backend systems. This expertise allows him to deliver reliable services that can scale effectively.`,
 		logo: Assets.api,
 		name: 'Restful APis',
 		category: 'devtools'
@@ -251,7 +256,9 @@ Wasif believes that just as learning programming in one language makes it easier
 	defineSkill({
 		slug: 'vite',
 		color: 'orange',
-		description: ``,
+		description: `Wasif Khan enjoys building frontend web applications using Vite, appreciating its reputation as one of the easiest and most popular tools among developers today. Vite significantly streamlines the local development environment, making it more efficient and enjoyable.
+
+He particularly values its hot module replacement feature, which allows for real-time updates without a full page refresh. This capability reduces development time and enhances productivity, enabling Wasif to see changes instantly as he works. His positive experience with Vite reflects his commitment to leveraging modern tools to create high-quality web applications.`,
 		logo: Assets.Vite,
 		name: 'Vite',
 		category: 'devtools'
@@ -259,7 +266,9 @@ Wasif believes that just as learning programming in one language makes it easier
 	defineSkill({
 		slug: 'photoshop',
 		color: 'orange',
-		description: ``,
+		description: `Wasif Khan began using Adobe Photoshop in 2013, starting with Adobe Photoshop 2005 to enhance pictures. As he grew up, he developed a solid understanding of the software. When he explored the updated versions of Photoshop, he found that most functionalities remained consistent, with only a few enhancements.
+
+On platforms like Fiverr, he completed jobs for clients, specializing in image restoration. Additionally, while he created logos in vector format, he often used Photoshop to enhance their visual appeal through lighting effects and other adjustments. This combination of skills in both Illustrator and Photoshop allows Wasif to create attractive and professional designs that meet his clients' needs.`,
 		logo: Assets.Photoshop,
 		name: 'Photoshop',
 		category: 'devtools'
@@ -267,7 +276,9 @@ Wasif believes that just as learning programming in one language makes it easier
 	defineSkill({
 		slug: 'Illustrator',
 		color: 'orange',
-		description: ``,
+		description: `Wasif Khan has two years of experience in graphic design, where he honed his design skills and developed a keen eye for symmetry. He created mockups that are showcased in his graphic design portfolio, demonstrating his proficiency and creativity.
+
+In 2019, he began using Adobe Illustrator to create SVG vectors and design logos, accumulating two years of solid experience with the tool. His strong design sensibility has enabled him to develop all the user interfaces for his projects independently, blending his graphic design expertise with his technical skills in web development. This unique combination allows him to create visually appealing and functional applications.`,
 		logo: Assets.Illustrator,
 		name: 'Illustrator',
 		category: 'devtools'
@@ -277,7 +288,7 @@ Wasif believes that just as learning programming in one language makes it easier
 export const title = 'Skills';
 
 export const getSkills = (
-	...slugs: Array<StringWithAutoComplete<(typeof items)[number]['slug']>>
+	...slugs: Array<StringWithAutoComplete<(typeof items)[number]['slug']>> 
 ): Array<Skill> => items.filter((it) => slugs.includes(it.slug));
 
 export const groupByCategory = (
