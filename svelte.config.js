4f632c72a4cc9a8693aeeea998bc134e8264461b
Wasif Khan <82050148+wasifkhan00@users.noreply.github.com>
2024-10-24 11:41:20 -0700

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 // commission 
-const base = '/';
+const base = '/wasif-khan-port';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
